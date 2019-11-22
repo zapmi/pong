ec98782747c55dc9671c1fff002916a5f779f43f
Mike Z <mzapata2991@gmail.com>
2019-11-21 21:05:21 -0600

    added seperation between player scores

--- pong.py
+++ pong.py
@@ -48,7 +48,7 @@ pen.color("green")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Player A: 0 | Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # function
 
@@ -106,14 +106,14 @@ while True:
         ball.dx *= -1 
         score_a += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Player A: {} | Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal")) 
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Player A: {} | Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal")) 
 
     #ball hits paddle
     if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
